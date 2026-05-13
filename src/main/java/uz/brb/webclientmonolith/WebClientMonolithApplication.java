3efa1ded1372b968b2a5c7e33889e553a1505d97
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2026-05-13 09:41:38 +0500

    web client

---
+++ src/main/java/uz/brb/webclientmonolith/WebClientMonolithApplication.java
@@ -0,0 +1,13 @@
+package uz.brb.webclientmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientMonolithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/brb/webclientmonolith/WebClientMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brb.webclientmonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
